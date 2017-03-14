1b9ac780152041801f2fb6201402dce9afc2cdf8
Teresa Yong <TeresaYong@Macs-MacBook-Pro.local>
2017-03-14 13:30:13 +0700

    webservices folder

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 */
 
 //add your code here...
-
+var alchemy_endpoint = "https://gateway-a.watsonplatform.net/calls/data/GetNews";
+var alchemy_key = "3e629e327b62f6719852cd843f757a773c6e7baf";
 
 // ------ don't change any code below here, but check it out. :)
 var api_url = alchemy_endpoint+"?apikey="+alchemy_key+"&outputMode=json&start=now-1d&end=now&count=10&return=enriched.url.url,enriched.url.title";
